b92050ce710051849e2b9cdbca8d9657597085b6
ftetaud <ftetaud@sodifrance.fr>
2018-11-20 11:43:25 +0100

    Release 5.2.3

--- static/js/6.js
+++ static/js/6.js
@@ -1,2 +1,218 @@
-webpackJsonp([6],{297:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r=n(0),u=n(116),o=n(567),i=n(568),c=n(569),a=n(20),s=function(t){function e(){var e=t.call(this)||this;return e.addDataRoute(a.URL_STATUS,function(){return new u.DataRouteInfos(i.StatusGet,null,c.CommonServiceDataImpl)},u.PUBLIC_ROUTE,"get"),e.addDataRoute(a.URL_FEATURES,function(){return new u.DataRouteInfos(o.FeaturesGetAction,null,c.CommonServiceDataImpl)},u.PUBLIC_ROUTE,"get"),e}return r.__extends(e,t),e}(u.AbstractRoutes);e.default=s},567:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r=n(0),u=n(116),o=n(1),i=(o.Utils.getLogger("applitutoriel.actions.common.features.get"),function(t){function e(){return null!==t&&t.apply(this,arguments)||this}return r.__extends(e,t),e.prototype.execute=function(){return this.doExecute()},e.prototype.doExecute=function(){return this.getService().getFeatures().then(function(t){var e={};return(t||[]).forEach(function(t){e[t.id]=r.__assign({},t)}),e}).catch(function(){return{}})},e}(u.RouteActionService));e.FeaturesGetAction=i},568:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r=n(0),u=n(116),o=n(1),i=o.Utils.getLogger("applitutoriel.actions.common.status-get"),c=function(t){function e(){return null!==t&&t.apply(this,arguments)||this}return r.__extends(e,t),e.prototype.execute=function(){return this.doExecute()},e.prototype.doExecute=function(){return this.getService().getStatus().then(function(t){i.debug("RESPONSE-OK",t);try{return t}catch(t){return i.error("RESPONSE-KO",t),[]}})},e}(u.RouteActionService);e.StatusGet=c},569:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r=n(0),u=n(13),o=n(1),i=n(19),c=n(20),a=n(570),s=(o.Utils.getLogger("applitutoriel.services.data.common.common-service-data-impl"),function(t){function e(){return null!==t&&t.apply(this,arguments)||this}return r.__extends(e,t),e.prototype.getStatus=function(){var t={url:this.buildUrl(c.URL_STATUS),method:"get",noCached:!0};return this.fetch(t)},e.prototype.getFeatures=function(){this.buildUrl(c.URL_FEATURES);return i.Promise.resolve(a)},e}(u.ServiceRequest));e.CommonServiceDataImpl=s},570:function(t,e){t.exports={featureFlipping:[{id:"partenaire",label:"Activation des partenaires",enabled:!0}]}}});
+webpackJsonp([6],{
+
+/***/ 314:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+/**
+ * Copyright ou © ou Copr. Ministère de l'Europe et des Affaires étrangères (2017)
+ * <p/>
+ * pole-architecture.dga-dsi-psi@diplomatie.gouv.fr
+ * <p/>
+ * Ce logiciel est un programme informatique servant à faciliter la création
+ * d'applications Web conformément aux référentiels généraux français : RGI, RGS et RGAA
+ * <p/>
+ * Ce logiciel est régi par la licence CeCILL soumise au droit français et
+ * respectant les principes de diffusion des logiciels libres. Vous pouvez
+ * utiliser, modifier et/ou redistribuer ce programme sous les conditions
+ * de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
+ * sur le site "http://www.cecill.info".
+ * <p/>
+ * En contrepartie de l'accessibilité au code source et des droits de copie,
+ * de modification et de redistribution accordés par cette licence, il n'est
+ * offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+ * seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+ * titulaire des droits patrimoniaux et les concédants successifs.
+ * <p/>
+ * A cet égard  l'attention de l'utilisateur est attirée sur les risques
+ * associés au chargement,  à l'utilisation,  à la modification et/ou au
+ * développement et à la reproduction du logiciel par l'utilisateur étant
+ * donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+ * manipuler et qui le réserve donc à des développeurs et des professionnels
+ * avertis possédant  des  connaissances  informatiques approfondies.  Les
+ * utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+ * logiciel à leurs besoins dans des conditions permettant d'assurer la
+ * sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+ * à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+ * <p/>
+ * Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+ * pris connaissance de la licence CeCILL, et que vous en avez accepté les
+ * termes.
+ * <p/>
+ * <p/>
+ * Copyright or © or Copr. Ministry for Europe and Foreign Affairs (2017)
+ * <p/>
+ * pole-architecture.dga-dsi-psi@diplomatie.gouv.fr
+ * <p/>
+ * This software is a computer program whose purpose is to facilitate creation of
+ * web application in accordance with french general repositories : RGI, RGS and RGAA.
+ * <p/>
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ * <p/>
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ * <p/>
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ * <p/>
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = __webpack_require__(0);
+/**
+ * applitutoriel-js - Application tutoriel utilisant le Framework hornet
+ *
+ * @author MEAE - Ministère de l'Europe et des Affaires étrangères
+ * @version v5.2.3
+ * @link git+https://github.com/diplomatiegouvfr/applitutoriel-modules.git
+ * @license CECILL-2.1
+ */
+var hornet_js_utils_1 = __webpack_require__(1);
+var ReferentielAction = __webpack_require__(63);
+var abstract_routes_1 = __webpack_require__(126);
+var roles_1 = __webpack_require__(360);
+var urls_1 = __webpack_require__(19);
+var ref_pays_service_1 = __webpack_require__(63);
+var injector_1 = __webpack_require__(65);
+var logger = hornet_js_utils_1.Utils.getLogger("applitutoriel.src.routes.ref.ref-routes");
+var ReferentielRoutes = /** @class */ (function (_super) {
+    tslib_1.__extends(ReferentielRoutes, _super);
+    function ReferentielRoutes() {
+        var _this = _super.call(this) || this;
+        _this.addDataRoute(urls_1.URL_REF_VILLES, function () { return new abstract_routes_1.DataRouteInfos(ReferentielAction.ListerVilles, null, injector_1.Injector.getRegistered(ref_pays_service_1.ReferentielPaysService)); }, roles_1.Roles.EVERYONE);
+        _this.addDataRoute(urls_1.URL_PAYS, function () { return new abstract_routes_1.DataRouteInfos(ReferentielAction.ListerPays, null, injector_1.Injector.getRegistered(ref_pays_service_1.ReferentielPaysService)); }, roles_1.Roles.EVERYONE);
+        _this.addDataRoute(urls_1.URL_REF_NATIONALITE, function () { return new abstract_routes_1.DataRouteInfos(ReferentielAction.ListerNationalites, null, injector_1.Injector.getRegistered(ref_pays_service_1.ReferentielPaysService)); }, roles_1.Roles.EVERYONE);
+        _this.addDataRoute(urls_1.URL_REF_NATIONALITE + "/recherche", function (nationalite) { return new abstract_routes_1.DataRouteInfos(ReferentielAction.ListerNationalites, null, injector_1.Injector.getRegistered(ref_pays_service_1.ReferentielPaysService)); }, roles_1.Roles.ADMIN, "post");
+        return _this;
+    }
+    return ReferentielRoutes;
+}(abstract_routes_1.AbstractRoutes));
+exports.default = ReferentielRoutes;
+
+
+
+/***/ }),
+
+/***/ 360:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+/**
+ * Copyright ou © ou Copr. Ministère de l'Europe et des Affaires étrangères (2017)
+ * <p/>
+ * pole-architecture.dga-dsi-psi@diplomatie.gouv.fr
+ * <p/>
+ * Ce logiciel est un programme informatique servant à faciliter la création
+ * d'applications Web conformément aux référentiels généraux français : RGI, RGS et RGAA
+ * <p/>
+ * Ce logiciel est régi par la licence CeCILL soumise au droit français et
+ * respectant les principes de diffusion des logiciels libres. Vous pouvez
+ * utiliser, modifier et/ou redistribuer ce programme sous les conditions
+ * de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
+ * sur le site "http://www.cecill.info".
+ * <p/>
+ * En contrepartie de l'accessibilité au code source et des droits de copie,
+ * de modification et de redistribution accordés par cette licence, il n'est
+ * offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+ * seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+ * titulaire des droits patrimoniaux et les concédants successifs.
+ * <p/>
+ * A cet égard  l'attention de l'utilisateur est attirée sur les risques
+ * associés au chargement,  à l'utilisation,  à la modification et/ou au
+ * développement et à la reproduction du logiciel par l'utilisateur étant
+ * donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+ * manipuler et qui le réserve donc à des développeurs et des professionnels
+ * avertis possédant  des  connaissances  informatiques approfondies.  Les
+ * utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+ * logiciel à leurs besoins dans des conditions permettant d'assurer la
+ * sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+ * à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+ * <p/>
+ * Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+ * pris connaissance de la licence CeCILL, et que vous en avez accepté les
+ * termes.
+ * <p/>
+ * <p/>
+ * Copyright or © or Copr. Ministry for Europe and Foreign Affairs (2017)
+ * <p/>
+ * pole-architecture.dga-dsi-psi@diplomatie.gouv.fr
+ * <p/>
+ * This software is a computer program whose purpose is to facilitate creation of
+ * web application in accordance with french general repositories : RGI, RGS and RGAA.
+ * <p/>
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ * <p/>
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ * <p/>
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ * <p/>
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * applitutoriel-js-common - Application tutoriel utilisant le Framework hornet
+ *
+ * @author MEAE - Ministère de l'Europe et des Affaires étrangères
+ * @version v5.2.3
+ * @link git+https://github.com/diplomatiegouvfr/applitutoriel-modules.git
+ * @license CECILL-2.1
+ */
+/**
+ * Classe regroupant les rôles disponibles dans l'appli-tuto
+ */
+var Roles = /** @class */ (function () {
+    function Roles() {
+    }
+    Roles.USER_STR = "APPLI_TUTO_USER";
+    Roles.ADMIN_STR = "APPLI_TUTO_ADMIN";
+    Roles.USER = [Roles.USER_STR];
+    Roles.ADMIN = [Roles.ADMIN_STR];
+    Roles.EVERYONE = [Roles.USER_STR, Roles.ADMIN_STR];
+    return Roles;
+}());
+exports.Roles = Roles;
+
+
+
+/***/ })
+
+});
 //# sourceMappingURL=6.js.map
